10ef98885a850a885ade5577852cec914f72f270
hsfeng <hsfeng1996@foxmail.com>
2019-09-16 08:49:03 +0800

    secondexample

---
+++ src/main/java/com/hsfeng/netty/secondexample/MyServerHandler.java
@@ -0,0 +1,24 @@
+package com.hsfeng.netty.secondexample;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.util.UUID;
+
+/**
+ * @Author hsfeng
+ * @Create 2019/9/16 12:42
+ */
+public class MyServerHandler extends SimpleChannelInboundHandler<String> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+        System.out.println(ctx.channel().remoteAddress() + ", " + msg);
+        ctx.channel().writeAndFlush("from server: " + UUID.randomUUID());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
