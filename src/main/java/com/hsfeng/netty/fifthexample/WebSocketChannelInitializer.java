0424f1112b100c17fdba2b7870e9d75fbc802acf
hsfeng <hsfeng1996@foxmail.com>
2019-09-24 22:24:48 +0800

    fifthexample

---
+++ src/main/java/com/hsfeng/netty/fifthexample/WebSocketChannelInitializer.java
@@ -0,0 +1,28 @@
+package com.hsfeng.netty.fifthexample;
+
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.codec.http.websocketx.extensions.compression.WebSocketServerCompressionHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+/**
+ * @Author hsfeng
+ * @Create 2019/9/23 15:47
+ */
+public class WebSocketChannelInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        pipeline.addLast(new HttpServerCodec());
+        pipeline.addLast(new ChunkedWriteHandler());
+        pipeline.addLast(new HttpObjectAggregator(8192));
+        pipeline.addLast(new WebSocketServerProtocolHandler("/ws"));
+        pipeline.addLast(new TextWebSocketFrameHandler());
+    }
+}
