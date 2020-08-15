2648067005450b2fb70bd362a36ef91666fe3f87
hsfeng <hsfeng@hsfeng.net>
2020-08-15 16:47:15 +0800

    thrift

---
+++ src/main/java/com/hsfeng/netty/thrift/PersonServiceImpl.java
@@ -0,0 +1,31 @@
+package com.hsfeng.netty.thrift;
+
+import com.google.gson.stream.JsonToken;
+import org.apache.thrift.TException;
+import thrift.generated.DataException;
+import thrift.generated.Person;
+import thrift.generated.PersonService;
+
+public class PersonServiceImpl implements PersonService.Iface {
+
+    @Override
+    public Person getPersonByUsername(String username) throws DataException, TException {
+        System.out.println("Got Client Param: " + username);
+
+        Person person = new Person();
+        person.setUsername(username);
+        person.setAge(20);
+        person.setMarried(false);
+
+        return person;
+    }
+
+    @Override
+    public void savePersion(Person person) throws DataException, TException {
+        System.out.println("Got Client Param: ");
+
+        System.out.println(person.getUsername());
+        System.out.println(person.getAge());
+        System.out.println(person.isMarried());
+    }
+}
