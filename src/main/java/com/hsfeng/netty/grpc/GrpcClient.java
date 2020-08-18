5b4f0d88fbd3c725c952c7dc4c44d896918a6b54
hsfeng <hsfeng@hsfeng.net>
2020-08-18 22:07:01 +0800

    grpc stream

--- src/main/java/com/hsfeng/netty/grpc/GrpcClient.java
+++ src/main/java/com/hsfeng/netty/grpc/GrpcClient.java
@@ -1,17 +1,85 @@
 package com.hsfeng.netty.grpc;
 
-import com.hsfeng.netty.proto.MyRequest;
-import com.hsfeng.netty.proto.MyResponse;
-import com.hsfeng.netty.proto.StudentServiceGrpc;
+import com.hsfeng.netty.proto.*;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+import jdk.nashorn.internal.ir.CallNode;
+
+import java.time.LocalDateTime;
+import java.util.Iterator;
 
 public class GrpcClient {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost", 8899).usePlaintext().build();
         StudentServiceGrpc.StudentServiceBlockingStub blockingStub = StudentServiceGrpc.newBlockingStub(managedChannel);
-        MyResponse myResponse = blockingStub.getRealNameByUsername(MyRequest.newBuilder().setUsername("zhangsan").build());
+        StudentServiceGrpc.StudentServiceStub stub = StudentServiceGrpc.newStub(managedChannel);
+
+        /*MyResponse myResponse = blockingStub.getRealNameByUsername(MyRequest.newBuilder().setUsername("zhangsan").build());
         System.out.println(myResponse.getRealname());
+
+        System.out.println("----------------");
+
+        Iterator<StudentResponse> iterator = blockingStub.getStudentsByAge(StudentRequest.newBuilder().setAge(20).build());
+        iterator.forEachRemaining(studentResponse -> {
+            System.out.println(studentResponse.getName() + ", " + studentResponse.getAge() + ", " + studentResponse.getCity());
+        });
+
+        System.out.println("----------------");*/
+
+        /*StreamObserver<StudentResponseList> studentResponseListStreamObserver = new StreamObserver<StudentResponseList>() {
+            @Override
+            public void onNext(StudentResponseList studentResponseList) {
+                studentResponseList.getStudentResponseList().forEach(studentResponse -> {
+                    System.out.println(studentResponse.getName());
+                    System.out.println(studentResponse.getAge());
+                    System.out.println(studentResponse.getCity());
+                    System.out.println("********");
+                });
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                System.out.println(throwable.getMessage());
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("completed");
+            }
+        };
+        StreamObserver<StudentRequest> studentRequestStreamObserver = stub.getStudentsWrapperByAges(studentResponseListStreamObserver);
+        studentRequestStreamObserver.onNext(StudentRequest.newBuilder().setAge(20).build());
+        studentRequestStreamObserver.onNext(StudentRequest.newBuilder().setAge(30).build());
+        studentRequestStreamObserver.onNext(StudentRequest.newBuilder().setAge(40).build());
+        studentRequestStreamObserver.onNext(StudentRequest.newBuilder().setAge(50).build());
+        studentRequestStreamObserver.onCompleted();*/
+
+        StreamObserver<StreamRequest> requestStreamObserver = stub.biTalk(new StreamObserver<StreamResponse>() {
+            @Override
+            public void onNext(StreamResponse streamResponse) {
+                System.out.println(streamResponse.getResponseInfo());
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                System.out.println(throwable.getMessage());
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("onCompleted");
+            }
+        });
+        for(int i=0;i<10;i++){
+            requestStreamObserver.onNext(StreamRequest.newBuilder().setRequestInfo(LocalDateTime.now().toString()).build());
+            Thread.sleep(1000);
+        }
+        requestStreamObserver.onCompleted();
+
+        while(true){
+            Thread.sleep(1000);
+        }
     }
 }
--- src/main/java/com/hsfeng/netty/grpc/StudentServiceImpl.java
+++ src/main/java/com/hsfeng/netty/grpc/StudentServiceImpl.java
@@ -1,9 +1,9 @@
 package com.hsfeng.netty.grpc;
 
-import com.hsfeng.netty.proto.MyRequest;
-import com.hsfeng.netty.proto.MyResponse;
-import com.hsfeng.netty.proto.StudentServiceGrpc;
+import com.hsfeng.netty.proto.*;
 import io.grpc.stub.StreamObserver;
+
+import java.util.UUID;
 
 public class StudentServiceImpl extends StudentServiceGrpc.StudentServiceImplBase {
 
@@ -14,5 +14,64 @@ public class StudentServiceImpl extends StudentServiceGrpc.StudentServiceImplBas
 
         responseObserver.onNext(MyResponse.newBuilder().setRealname("张三").build());
         responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getStudentsByAge(StudentRequest request, StreamObserver<StudentResponse> responseObserver) {
+        System.out.println("接收到客户端信息：" + request.getAge());
+
+        responseObserver.onNext(StudentResponse.newBuilder().setName("张三").setAge(20).setCity("北京").build());
+        responseObserver.onNext(StudentResponse.newBuilder().setName("李四").setAge(30).setCity("天津").build());
+        responseObserver.onNext(StudentResponse.newBuilder().setName("王五").setAge(40).setCity("成都").build());
+        responseObserver.onNext(StudentResponse.newBuilder().setName("赵六").setAge(50).setCity("武汉").build());
+
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public StreamObserver<StudentRequest> getStudentsWrapperByAges(StreamObserver<StudentResponseList> responseObserver) {
+        return new StreamObserver<StudentRequest>() {
+            @Override
+            public void onNext(StudentRequest studentRequest) {
+                System.out.println("onNext: " + studentRequest.getAge());
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                System.out.println(throwable.getMessage());
+            }
+
+            @Override
+            public void onCompleted() {
+                StudentResponse studentResponse = StudentResponse.newBuilder().setName("张三").setAge(20).setCity("西安").build();
+                StudentResponse studentResponse2 = StudentResponse.newBuilder().setName("李四").setAge(30).setCity("深圳").build();
+
+                StudentResponseList studentResponseList = StudentResponseList.newBuilder().addStudentResponse(studentResponse).addStudentResponse(studentResponse2).build();
+
+                responseObserver.onNext(studentResponseList);
+                responseObserver.onCompleted();
+            }
+        };
+    }
+
+    @Override
+    public StreamObserver<StreamRequest> biTalk(StreamObserver<StreamResponse> responseObserver) {
+        return new StreamObserver<StreamRequest>() {
+            @Override
+            public void onNext(StreamRequest streamRequest) {
+                System.out.println(streamRequest.getRequestInfo());
+                responseObserver.onNext(StreamResponse.newBuilder().setResponseInfo(UUID.randomUUID().toString()).build());
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                System.out.println(throwable.getMessage());
+            }
+
+            @Override
+            public void onCompleted() {
+                responseObserver.onCompleted();
+            }
+        };
     }
 }
